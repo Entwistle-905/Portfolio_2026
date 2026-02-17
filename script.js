c1852d2839c92212d37bf17e82514fa33bff08fc
mac2648 <60781347+mac2648@users.noreply.github.com>
2026-02-17 15:16:55 -0500

    added functions to allow project images to be opened with creating a new tab

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ document.getElementById("downloadBtn").addEventListener("click", () => {
 
 });
 
+//getting the image modal  used to zoom images and binding events to it
+let imageModal = document.getElementById("image-modal")
+let projectImages = null;
+let CurrentImageIndex = 0;
+
+imageModal.onclick = (e) => {
+    if (e.target === imageModal) imageModal.style.display = "none";
+};
+
+document.addEventListener("keydown", (e) => {
+    if (imageModal.style.display !== "block") return;
+
+    if (e.key === "ArrowLeft") OpenImage(CurrentImageIndex - 1);
+    if (e.key === "ArrowRight") OpenImage(CurrentImageIndex + 1);
+    if (e.key === "Escape") imageModal.style.display = "none";
+});
+
+// Close modal
+document.getElementById("image-modal-close").onclick = () => imageModal.style.display = "none";
+
+// Buttons
+document.getElementById("imageModalPrevImage").onclick = () => OpenImage(CurrentImageIndex - 1);
+document.getElementById("imageModalNextImage").onclick = () => OpenImage(CurrentImageIndex + 1);
+
 
 /* PROJECT VIEWER */
 
@@ -28,16 +52,17 @@ function openProject(title, cover, video, images, desc){
   const gallery = document.getElementById("modalGallery");
   gallery.innerHTML = "";
 
-  images.forEach(img => {
+  projectImages = images;
+  images.forEach((img, index) => {
 
     const image = document.createElement("img");
     image.src = img;
 
     image.onclick = () => {
-      window.open(img, "_blank");
+      OpenImage(index);
     };
 
-    gallery.appendChild(image);
+    gallery.appendChild(image, index);
 
   });
 
@@ -49,5 +74,22 @@ function closeModal(){
 
   document.getElementById("modal").style.display = "none";
   document.getElementById("modalVideo").src = "";
+
+}
+
+function OpenImage(index){
+
+  if (index < 0){
+    index = projectImages.length - 1;
+  }
+
+  if (index >= projectImages.length){
+    index = 0;
+  }
+
+  imageModal.style.display = "block";
+  let ZoomImage = document.getElementById("modal-img");
+  ZoomImage.src = projectImages[index];
+  CurrentImageIndex = index;
 
 }
